6992218f83f566efc6c2dc4027075d7ef637b510
Bright Sakyi <ofosubrightsakyi@gmail.com>
2021-10-29 15:17:56 +0000

    Login and Signup(Main)

---
+++ screens/components/colors.js
@@ -0,0 +1,13 @@
+
+
+//colors
+const colors = {
+    dark: "#000",
+    light: "#a5a5a5",
+    white: "#fff",
+    primary: "#28388f",
+    secondary: "#64beff",
+    pink: "#ff2d5f"
+};
+
+export default colors;
