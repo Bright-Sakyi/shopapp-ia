8e7556c1ede63c8d9ac04442bb5fd214f2c8bc11
Bright-Sakyi <ofosubrightsakyi@gmail.com>
2021-11-03 23:05:08 +0000

    Products updated(main)

--- assets/products.js
+++ assets/products.js
@@ -5,26 +5,99 @@ const products = [
         price: "58",
         like: true,
         img: require("../assets/products/casual w.jpg"),
-        details: 
+        details: "fjjfjf"
     },
 
     {
         id: 2,
-        name: "Ladies Casual wear",
-        price: "58",
+        name: "Flare",
+        price: "51",
         like: false,
-        img: require("../assets/products/casual w.jpg"),
-        details: 
+        img: require("../assets/products/flare w.jpg"),
+        details: "dfhfbfb"
     },
 
     {
         id: 3,
-        name: "Ladies Casual wear",
-        price: "58",
+        name: "Short sleeved",
+        price: "65.99",
         like: false,
-        img: require("../assets/products/casual w.jpg"),
-        details: 
+        img: require("../assets/products/short sleeved w.jpg"),
+        details: "gnfufhf"
     },
+    {
+        id: 4,
+        name: "No sleeve",
+        price: "98.48",
+        like: true,
+        img: require("../assets/products/sleevless w.jpg"),
+        details: "dhdhd"
+    },
+    {
+        id: 5,
+        name: "Summer wear",
+        price: "160.45",
+        like: true,
+        img: require("../assets/products/summer dress w.jpg"),
+        details: "fjfjf"
+    },
+    {
+        id: 6,
+        name: "V  wear",
+        price: "125",
+        like: true,
+        img: require("../assets/products/vneck w.jpg"),
+        details: "fhjfjf"
+    },
+    {
+        id: 7,
+        name: "Jeans",
+        price: "30.25",
+        like: false,
+        img: require("../assets/products/jeans m.jpg"),
+        details: "ffjfhf"
+    },
+    {
+        id: 8,
+        name: "Polo Shirt",
+        price: "45",
+        like: true,
+        img: require("../assets/products/Polo neck m.jpg"),
+        details: "ryrtryr"
+    },
+    {
+        id: 9,
+        name: "Ripped Jeans",
+        price: "28.29",
+        like: false,
+        img: require("../assets/products/Ripped jeans m.jpg"),
+        details: "fhjfhfjhf"
+    },
+    {
+        id: 10,
+        name: "Men's Shirt",
+        price: "78.99",
+        like: false,
+        img: require("../assets/products/shirt m.jpg"),
+        details: "fjfjfjf"
+    },
+    {
+        id: 11,
+        name: "Sports wear",
+        price: "58.99",
+        like: false,
+        img: require("../assets/products/Sportspants m.jpg"),
+        details: "ffjfjfjf"
+    },
+    {
+        id: 12,
+        name: "Vintage Shirtr",
+        price: "100",
+        like: true,
+        img: require("../assets/products/Vintage shirt m.jpg"),
+        details: "fjfjfjfjf"
+    },
+
 
 
 ]
